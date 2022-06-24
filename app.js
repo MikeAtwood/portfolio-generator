3232dd8919e4165c7270c18ad1121d0afc2ce1d2
Deadpool1 <atwood169@yahoo.com>
2022-06-24 18:58:09 -0500

    added promises

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
-/*const pageHTML = generatePage(name, github);
+//const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
+fs.writeFile('./dist/index.html', pageHTML, err => {
+  if (err) {
+  throw err;
+  return;
+}
+console.log('Page created! Check out index.html in this directory to see it!');
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-}); */
+fs.copyFile('./src/style.css', './dist/style,css', err => {
+    if (err) {
+        console.log(err);
+        return;
+}
+console.log('Style sjeet copied successfully!');
+
+  //console.log('Portfolio complete! Check out index.html to see the output!');
+}); 
+});
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -136,13 +148,20 @@ const promptProject = portfolioData => {
 };
 
 promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        
-        // will be uncommented in lesson 4
-        const pageHTML = generatePage(portfolioData);
-        // fs.writeFile('./index.html', pageHTML, err => {
-        //   if (err) throw new Error(err);
-        //   console.log('Page created! Check out index.html in this directory to see it!');
-        // });
-    });
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
